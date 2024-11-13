2df0c70f0ab9fe5e0120d0519eb918336542bfad
DishantKanak <dishantkanakpvt@gmail.com>
2024-11-13 15:54:38 +0530

    Swich Case

---
+++ swich case.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+
+    int month=1;
+
+    swich(month);
+    {
+        case:1
+        printf("january");
+        break;
+
+         case:2
+        printf("february");
+        break;
+
+         case:3
+        printf("march");
+        break;
+
+         case:4
+        printf("april");
+        break;
+
+         case:5
+        printf("may");
+        break;
+
+         case:6
+        printf("june");
+        break;
+
+         case:7
+        printf("july");
+        break;
+
+         case:8
+        printf("augast");
+        break;
+
+         case:9
+        printf("saptember");
+        break;
+
+         case:10
+        printf("octobar");
+        break;
+
+         case:11
+        printf("navember");
+        break;
+
+
+         case:12
+        printf("disember");
+        break;
+
+    }
